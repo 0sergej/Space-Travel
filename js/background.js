f577e90df31deff70ef196296fb2e41a5c88bf34
0sergej <sergej2stojanovic@gmail.com>
2022-03-01 21:09:20 +0100

    Background loading js file
    Javascript file for loading the background.

---
+++ js/background.js
@@ -0,0 +1,32 @@
+const INDEX = document.querySelector('body')
+const DESTINATION = document.querySelector('body')
+// const CREW = document.querySelector('body')
+// const TECHNOLOGY = document.querySelector('body')
+
+INDEX.style.setProperty(
+    'background-image',
+    'url(../assets/home/background-home-desktop.jpg)'
+)
+INDEX.style.setProperty('background-repeat', 'no-repeat')
+INDEX.style.setProperty('background-size', 'cover')
+
+DESTINATION.style.setProperty(
+    'background-image',
+    'url(../assets/destination/background-destination-desktop.jpg)'
+)
+DESTINATION.style.setProperty('background-repeat', 'no-repeat')
+DESTINATION.style.setProperty('background-size', 'cover')
+
+// CREW.style.setProperty(
+//     'background-image',
+//     'url(../assets/home/background-home-desktop.jpg)'
+// )
+// CREW.style.setProperty('background-repeat', 'no-repeat')
+// CREW.style.setProperty('background-size', 'cover')
+
+// TECHNOLOGY.style.setProperty(
+//     'background-image',
+//     'url(../assets/home/background-home-desktop.jpg)'
+// )
+// TECHNOLOGY.style.setProperty('background-repeat', 'no-repeat')
+// TECHNOLOGY.style.setProperty('background-size', 'cover')
