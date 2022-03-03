9b59f9c70386bfc2ab8c040f040117cf6dc6aa24
0sergej <sergej2stojanovic@gmail.com>
2022-03-03 16:22:27 +0100

    Crew page js
    Added background to Crew page through js file. Made dataCrew.js file to add data to html.

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 const INDEX = document.querySelector('body')
 const DESTINATION = document.querySelector('body')
-// const CREW = document.querySelector('body')
+const CREW = document.querySelector('body')
 // const TECHNOLOGY = document.querySelector('body')
 
 INDEX.style.setProperty(
@@ -17,16 +17,16 @@ DESTINATION.style.setProperty(
 DESTINATION.style.setProperty('background-repeat', 'no-repeat')
 DESTINATION.style.setProperty('background-size', 'cover')
 
-// CREW.style.setProperty(
-//     'background-image',
-//     'url(../assets/home/background-home-desktop.jpg)'
-// )
-// CREW.style.setProperty('background-repeat', 'no-repeat')
-// CREW.style.setProperty('background-size', 'cover')
+CREW.style.setProperty(
+    'background-image',
+    'url(../assets/crew/background-crew-desktop.jpg)'
+)
+CREW.style.setProperty('background-repeat', 'no-repeat')
+CREW.style.setProperty('background-size', 'cover')
 
 // TECHNOLOGY.style.setProperty(
 //     'background-image',
-//     'url(../assets/home/background-home-desktop.jpg)'
+//     'url(../assets/technology/background-technology-desktop.jpg)'
 // )
 // TECHNOLOGY.style.setProperty('background-repeat', 'no-repeat')
 // TECHNOLOGY.style.setProperty('background-size', 'cover')
